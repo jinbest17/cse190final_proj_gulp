c52e396bef479924df2f5370e45fdf1e6ae13119
Jinhao <jinhaoliu18@hotmail.com>
2020-06-11 19:14:16 -0700

    finished entire pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,36 @@
 const gulp = require('gulp');
 const image = require('gulp-image');
 const clean = require('gulp-clean');
-
+//const copy = require('gulp-copy');
+const cleanCSS = require('gulp-clean-css'); 
+const del = require('del');
+const uglify = require('gulp-uglify')
 // clean build directory
-gulp.task('clean', function (){
-    gulp.src('./build', {read: false})
-        .pipe(clean({force: true}));
+gulp.task('clean', function (done){
+  del('build/**', {force: true});
+  done();
+});
+
+// copy all files
+gulp.task('copy', function() {
+  gulp.src('./catalog/**')
+    .pipe(gulp.dest('./build/catalog'));
+  gulp.src('./_about/**')
+    .pipe(gulp.dest('./build/_about'));
+  gulp.src(['./_resources/bootstrap/**'])
+    .pipe(gulp.dest('./build/_resources/bootstrap'));
+  gulp.src(['./_resources/fonts/**'])
+    .pipe(gulp.dest('./build/_resources/fonts'));
+  gulp.src('./academics/**')
+    .pipe(gulp.dest('./build/academics'));
+  gulp.src('./common/**')
+    .pipe(gulp.dest('./build/common'));
+  gulp.src('./about/**')
+    .pipe(gulp.dest('./build/about'));
+  gulp.src('./favicon.ico')
+    .pipe(gulp.dest('./build'));
+  gulp.src('./index.html')
+    .pipe(gulp.dest('./build'));
 });
 
 // minify images with customized settings
@@ -34,4 +59,20 @@ gulp.task('image', async () => {
       .pipe(gulp.dest('./build/_resources/img'));
   });
 
-gulp.task('build', ['clean','image']);
+  gulp.task('minify-css', () => {
+    // Folder with files to minify
+    return gulp.src('./_resources/css/**/*.css')
+    //The method pipe() allow you to chain multiple tasks together 
+    //I execute the task to minify the files
+   .pipe(cleanCSS())
+   //I define the destination of the minified files with the method dest
+   .pipe(gulp.dest('./build/_resources/css'));
+  });
+
+  gulp.task('uglify-js', function() {
+    return gulp.src('./_resources/js/**')
+                .pipe(uglify())
+                .pipe(gulp.dest('./build/_resources/js'))
+  })
+
+  gulp.task('build', ['clean', 'copy', 'minify-css', 'image', 'uglify-js']);
